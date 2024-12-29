7c4b5587e5fe195baabfc46206057ed66868a10b
Ayumu3746221 <a.kkt0307@gmail.com>
2024-12-29 13:12:18 +0900

    add @ts-ignore to submit.ts

--- sampleReactHooks/src/lib/submit.ts
+++ sampleReactHooks/src/lib/submit.ts
@@ -4,21 +4,20 @@ interface QueryData {
   get(key: string): string | null;
 }
 
-interface prevState {
+interface response {
   state: boolean;
   message: string;
 }
-
 interface response {
   state: boolean;
   message: string;
 }
 
 export async function submitForm(
-  prevState?: prevState | null,
+  // @ts-ignore
+  state: response | null,
   queryData?: QueryData
 ): Promise<response> {
-  console.log("submitForm", prevState);
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
   const userName = queryData?.get("userName");
@@ -26,18 +25,14 @@ export async function submitForm(
   const comment = queryData?.get("comment");
 
   if (!userName || !age || !comment) {
-    prevState = {
+    return {
       state: false,
       message: "Please fill in all fields.",
     };
-
-    return prevState;
   }
 
-  prevState = {
+  return {
     state: true,
     message: "Form submitted successfully.",
   };
-
-  return prevState;
 }
